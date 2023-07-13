4fded1ab7a835c536ae0902e7f94b2be67a935f4
asia898 <joanna.kwiatkowska989@gmail.com>
2023-07-13 13:18:14 +0200

    Creating a recipe model. Adding layout to the components. Creating header. Creating layout for the recipe item

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'This is simply a test', 'https://realfood.tesco.com/media/images/Paella1400x919--921069c0-e3c1-46c2-a7fb-da21bf7bcd44-0-1400x919.jpg'),
+    new Recipe('Paella', 'This is simply a test', 'https://realfood.tesco.com/media/images/Paella1400x919--921069c0-e3c1-46c2-a7fb-da21bf7bcd44-0-1400x919.jpg')
+  ];
 
+  constructor() {
+
+  }
+
+  ngOnInit() {}
 }
