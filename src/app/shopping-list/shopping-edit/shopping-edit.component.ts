b353845521c6a047fbc1fd4136972b9826735e5d
asia898 <joanna.kwiatkowska989@gmail.com>
2023-07-20 12:49:13 +0200

    Adding new ingredient feature: My solution

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent {
+  @ViewChild('amountInput', {static: true}) newAmount: ElementRef;
 
+  @Output() newIngredientAdded = new EventEmitter<Ingredient>();
+
+  onAdd(nameInput:HTMLInputElement) {
+    this.newIngredientAdded.emit(new Ingredient(nameInput.value, this.newAmount.nativeElement.value));
+    console.log('Added pressed name: ' + nameInput.value + ' amount: ' + this.newAmount.nativeElement.value);
+
+  }
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -19,4 +19,8 @@ export class ShoppingListComponent {
   ngOnInit() {
 
   }
+
+  onNewIgredient(newIngredient: Ingredient) {
+    this.ingredients.push(newIngredient);
+  }
 }
