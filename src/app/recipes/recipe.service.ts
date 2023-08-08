351f0fa1d15a5a5a78ca68c6dbc63e0fffb97f8f
asia898 <joanna.kwiatkowska989@gmail.com>
2023-08-08 10:07:30 +0200

    Adding ingredients to the recipes.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,31 @@
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 import { Recipe } from './recipe.model';
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A test recipe', 'This is simply a test', 'https://realfood.tesco.com/media/images/Paella1400x919--921069c0-e3c1-46c2-a7fb-da21bf7bcd44-0-1400x919.jpg'),
-        new Recipe('Paella', 'This is simply a test', 'https://realfood.tesco.com/media/images/Paella1400x919--921069c0-e3c1-46c2-a7fb-da21bf7bcd44-0-1400x919.jpg')
+        new Recipe(
+            'Tasty Schnitzel',
+            'This is simply a test',
+            'https://realfood.tesco.com/media/images/Paella1400x919--921069c0-e3c1-46c2-a7fb-da21bf7bcd44-0-1400x919.jpg',
+            [
+                new Ingredient('Meat', 1),
+                new Ingredient('French Fries', 20)
+            ]
+            ),
+        new Recipe(
+            'Paella',
+            'This is simply a test',
+            'https://realfood.tesco.com/media/images/Paella1400x919--921069c0-e3c1-46c2-a7fb-da21bf7bcd44-0-1400x919.jpg',
+            [
+                new Ingredient('Rice', 1),
+                new Ingredient('Sea Food', 15),
+                new Ingredient('Checken wings', 4),
+                new Ingredient('Garlic', 3),
+            ]
+            )
       ];
 
     getRecipes() {
