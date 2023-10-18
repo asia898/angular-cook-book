7baeb39620d792cbfb457d3ea9d013a9f2b313bb
asia898 <joanna.kwiatkowska989@gmail.com>
2023-10-18 16:17:22 +0200

    Cleaning up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,4 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'cook-book';
   loadedFeature = 'recipe';
-
-  onNavigate(feature: string) {
-    this.loadedFeature = feature;
-  }
 }
