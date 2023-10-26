7d91f73332220ccc77a6aec2efe9fc19db6188bd
asia898 <joanna.kwiatkowska989@gmail.com>
2023-10-26 16:31:51 +0200

    Using rxjs Subject instead of event emitter in shopping list service.

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,8 +1,8 @@
-import { EventEmitter } from "@angular/core";
+import { Subject } from "rxjs";
 import { Ingredient } from "../shared/ingredient.model";
 
 export class ShoppingListService {
-    ingredientsChanged = new EventEmitter<Ingredient[]>();
+    ingredientsChanged = new Subject<Ingredient[]>();
     private ingredients: Ingredient[] = [
         new Ingredient('Apples', 5),
         new Ingredient('Tomatoes', 10)
@@ -14,12 +14,12 @@ export class ShoppingListService {
 
     addIngredient(newIngredient: Ingredient) {
         this.ingredients.push(newIngredient);
-        this.ingredientsChanged.emit(this.ingredients.slice());
+        this.ingredientsChanged.next(this.ingredients.slice());
     }
 
-    // Separate method to add various ingredients so that we do not emit too many events.
+    // Separate method to add various ingredients so that we do not next too many events.
     addIngredients(newIngredients: Ingredient[]) {
         this.ingredients.push(...this.ingredients);
-        this.ingredientsChanged.emit(this.ingredients.slice());
+        this.ingredientsChanged.next(this.ingredients.slice());
     }
 }
