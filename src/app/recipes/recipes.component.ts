0b8acff36526d71df4b2331f03b4892e076f72f7
asia898 <joanna.kwiatkowska989@gmail.com>
2023-10-26 16:48:25 +0200

    Cleaning up the components.

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Recipe } from './recipe.model';
-
 import { RecipeService } from './recipe.service';
 
 @Component({
@@ -11,16 +9,9 @@ import { RecipeService } from './recipe.service';
   providers: [RecipeService]
 })
 export class RecipesComponent implements OnInit{
-  recSelected: Recipe;
-
-  constructor(private recipeService: RecipeService) {}
+  constructor() {}
 
   ngOnInit(): void {
-    this.recipeService.recipeSelected.subscribe(
-      (recipe: Recipe) => {
-        this.recSelected = recipe;
-      }
-    );
   }
 
 }
