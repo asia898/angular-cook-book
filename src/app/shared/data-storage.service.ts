70443d303fb7adfd3fbfe2bdd13a5b885f892382
asia898 <joanna.kwiatkowska989@gmail.com>
2025-04-23 10:40:24 +0200

    Soring all recipes to the database

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,10 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { RecipeService } from '../recipes/recipe.service';
 
 @Injectable({providedIn: 'root'})
 
 export class DataStorageService {
-    constructor(private http: HttpClient) {
+    constructor(private http: HttpClient, private recipeService: RecipeService) {}
+    
+    storeRecipes() {
+        const recipes = this.recipeService.getRecipes();
 
+        return this.http.put(
+            'https://cook-book-backend-6660c-default-rtdb.europe-west1.firebasedatabase.app/recipes.json'
+            ,recipes)
+            .subscribe(response => {
+                console.log(response);
+            });
     }
 }
